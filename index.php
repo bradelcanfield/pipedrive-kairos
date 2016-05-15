438899b89464ba84fbf1d2106a8f07878ae71c66
Bradel Canfield <bradel.canfield@gmail.com>
2016-05-15 13:51:52 -0400

    storing  into data.php file

--- index.php
+++ index.php
@@ -12,8 +12,10 @@
   </head>
   <body>
 <?php
+// require "inc/data.php";
 require "inc/Wufoo-PHP-API-Wrapper/WufooApiWrapper.php";
 require "inc/pipedrive.php";
+require "index.html";
 // Wufoo
 $apiKey = '6393-IH1D-O54P-6NB1';
 $subdomain = 'bradelcanfield';
@@ -30,6 +32,9 @@ $data = json_decode($someJSON);
 // looping through all of the data to only give back the first name and last name fields of all form entries
 foreach ($form_data as $key => $value) {
     echo $value["Field1"] . "\n" . $value["Field2"] . "<br>";
+    $lead_name_data[] = $value["Field1"] . "\n" . $value["Field2"];
+    //storing select data as an array in a separate data.txt file
+    file_put_contents('data.txt', var_export($lead_name_data, true));
 }
 
 $first_name = $value["Field1"];
@@ -37,25 +42,26 @@ $last_name = $value["Field2"];
 
 // ******************************************************************************************************
 // Pipedrive
-$url = 'https://api.pipedrive.com/v1/persons?api_token=0f6acf75a1d4bddda9f46fa6c30f4e675e806d3f';
+// $url = 'https://api.pipedrive.com/v1/persons?api_token=0f6acf75a1d4bddda9f46fa6c30f4e675e806d3f';
 
-$lead_name = array('name' => $first_name . "\n" . $last_name);
-// Build Http query using $lead_name params
-$query = http_build_query($lead_name);
-// Create Http context details
-$contextData = array(
-    'method' => 'POST',
-    'header' => "Connection: close\r\n" .
-    "Content-Length: " . strlen($query) . "\r\n",
-    'content' => $query);
-// Create context resource for the request
-$context = stream_context_create(array('http' => $contextData));
-// Read page rendered as result of the POST request
-$result = file_get_contents(
-    $url, // page url
-    false,
-    $context);
+// $lead_name = array('name' => $first_name . "\n" . $last_name);
+// // Build Http query using $lead_name params
+// $query = http_build_query($lead_name);
+// // Create Http context details
+// $contextData = array(
+//     'method' => 'POST',
+//     'header' => "Connection: close\r\n" .
+//     "Content-Length: " . strlen($query) . "\r\n",
+//     'content' => $query);
+// // Create context resource for the request
+// $context = stream_context_create(array('http' => $contextData));
+// // Read page rendered as result of the POST request
+// $result = file_get_contents(
+//     $url, // page url
+//     false,
+//     $context);
 
 ?>
+<h1>hello</h1>
   </body>
 </html>
